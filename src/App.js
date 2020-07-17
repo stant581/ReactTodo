7010d6ef5118a435e70bd6d4509deaf9c48519af
Animesh Banerjee <stant581@gmail.com>
2020-07-17 15:51:51 +0530

    Deleting a ToDo

--- src/App.js
+++ src/App.js
@@ -10,11 +10,18 @@ class App extends Component {
     ]
   }
 
+  deleteTodo = (id) => {
+    let todos = this.state.todos.filter( todo =>{
+      return todo.id !== id
+    });
+    this.setState({todos});
+  }
+
   render(){
     return (
-      <div className="App">
+      <div className="App container">
       <h1 className="center blue-text"> Todo's</h1>
-      <Todos todos= {this.state.todos} />
+      <Todos todos= {this.state.todos} deleteTodo = {this.deleteTodo} />
       </div>
     );
   }
--- src/Todos.js
+++ src/Todos.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Todos = ({todos}) => {
+const Todos = ({todos, deleteTodo}) => {
 
     //if(todos.length > 0)
     //{
         const todoList = todos.length ? (todos.map( todo => {
             return (
                 <div className="collection-item" key={todo.id}>
-                    <span>{todo.content}</span>
+                    <span onClick = { () => {deleteTodo(todo.id)}}>{todo.content}</span>
+                    
                 </div>
             )
         })
