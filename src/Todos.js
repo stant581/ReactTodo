e71c65ccc657a3ac5df218e92e8b07137f82b35d
Animesh Banerjee <stant581@gmail.com>
2020-07-17 15:30:29 +0530

    The same thing with ternary Operators

--- src/Todos.js
+++ src/Todos.js
@@ -2,29 +2,30 @@ import React from 'react';
 
 const Todos = ({todos}) => {
 
-    if(todos.length > 0)
-    {
-        const todoList = todos.map( todo => {
+    //if(todos.length > 0)
+    //{
+        const todoList = todos.length ? (todos.map( todo => {
             return (
                 <div className="collection-item" key={todo.id}>
                     <span>{todo.content}</span>
                 </div>
             )
         })
+        ) : (<p className="center">No more Todo's</p>)
     return (
         <div className="todos collection">
         {todoList}
         </div>
     )
-    }
-    else{
-        const todoList = (<p className="center">No more Todo's</p>)
-        return (
-            <div className="todos collection">
-            {todoList}
-            </div>
-        )
-    }
+    //}
+    //else{
+    //    const todoList = (<p className="center">No more Todo's</p>)
+    //    return (
+     //       <div className="todos collection">
+      //      {todoList}
+       //     </div>
+        //)
+    //}
     
 }
 
