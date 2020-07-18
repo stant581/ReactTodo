42889221f4fdf24e6174babb35dd79ecc032235a
Animesh Banerjee <stant581@gmail.com>
2020-07-18 10:20:31 +0530

    Adding Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Todos from './Todos';
+import AddTodo from './Addform';
 
 class App extends Component {
 
@@ -17,11 +18,18 @@ class App extends Component {
     this.setState({todos});
   }
 
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({todos});
+  }
+
   render(){
     return (
       <div className="App container">
       <h1 className="center blue-text"> Todo's</h1>
       <Todos todos= {this.state.todos} deleteTodo = {this.deleteTodo} />
+      <AddTodo addTodo = {this.addTodo}/>
       </div>
     );
   }
